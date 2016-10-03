e1578a57e91fd296eace9b8f4f06c9b316e6f86c
Kristen Kehlenbeck <kristen@attend.com>
2016-10-03 16:41:52 -0400

    Completes endpoint to get all programs
    - Marketo sends programs in batches of 200, so if we receive a list of 200 programs, we'll recursively call getPrograms() again, but will include an offset of 200 in the options array
    - Will need to do some error handling here, because if we receive an "errors" key in the response body, we won't have any programs to parse through

--- src/API/Programs.php
+++ src/API/Programs.php
@@ -4,10 +4,34 @@ namespace Kristenlk\Marketo\API;
 use GuzzleHttp\Client as Guzzle;
 
 class Programs extends BaseClient {
-    public function getAllPrograms(){
-        $endpoint = '/rest/asset/v1/programs.json?maxReturn=200';
+    protected $programs = array();
 
-        $this->request("get", $endpoint);
+    public function getPrograms(Array $options = array()){
+        if ($options) {
+            $endpoint = '/rest/asset/v1/programs.json?maxReturn=200&offset=' . $options['offset'];
+        } else {
+            $endpoint = '/rest/asset/v1/programs.json?maxReturn=200';
+        }
+
+        $response = $this->request("get", $endpoint);
+
+        // If $response->getStatusCode() !== 200, throw an error
+        // If $responseBody->errors, return errors - don't iterate through $responseBody->result
+
+        $responseBody = $this->getBodyObjectFromResponse($response);
+
+        foreach ($responseBody->result as $program) {
+            array_push($this->programs, $program);
+        }
+
+        if (count($responseBody->result) == 200) {
+            if ($options) {
+                $this->getPrograms(array_merge($options, ["offset" => $options["offset"] + 200]));
+            } else {
+                $this->getPrograms(["offset" => 200]);
+            }
+        }
+        return $this->programs;
     }
 }
 ?>
